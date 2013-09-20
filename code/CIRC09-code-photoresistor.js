37ecc13f561fc3c94651181a445869dfea55bc0d
Anna Gerber <anna.m.gerber@gmail.com>
2013-09-20 16:59:14 +1000

    Code for circuit 9 - photoresistor

---
+++ code/CIRC09-code-photoresistor.js
@@ -0,0 +1,21 @@
+var five = require("johnny-five"),
+    board, myPhotoresistor, myLed;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  myLed = new five.Led(9);
+
+  myPhotoresistor = new five.Sensor({
+    pin: "A0",
+    freq: 250
+  });
+
+  
+  myPhotoresistor.on("read", function( err, value ) {
+    // range of led brightness is 0 - 255
+    var brightnessValue = five.Fn.constrain(five.Fn.map(value, 0, 900, 0, 255), 0, 255);
+    myLed.brightness(brightnessValue);
+  });
+});

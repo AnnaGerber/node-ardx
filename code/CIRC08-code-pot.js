ab67be94b36bf565596efc2d7e04b3a25a914b5e
Anna Gerber <anna.m.gerber@gmail.com>
2013-09-20 22:20:01 +1000

    rename var for consistency with other examples

--- code/CIRC08-code-pot.js
+++ code/CIRC08-code-pot.js
@@ -1,18 +1,18 @@
 var five = require("johnny-five"),
-    board, potentiometer;
+    board, myPotentiometer;
 
 board = new five.Board();
 
 board.on("ready", function() {
 
-  potentiometer = new five.Sensor({
+  myPotentiometer = new five.Sensor({
     pin: "A0",
     freq: 250
   });
 
   myLed = new five.Led(9);
 
-  potentiometer.on("read", function() {
+  myPotentiometer.on("read", function() {
     var rawValue = this.raw;
     myLed.brightness(Math.floor(rawValue / 4));
   });

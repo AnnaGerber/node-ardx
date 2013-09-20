cbe40f0b37bc2a06af984337aa81d22ecad59baa
Anna Gerber <anna.m.gerber@gmail.com>
2013-09-20 19:29:06 +1000

    Code for circuit 14 - Soft Pot controlling RGB LED

---
+++ code/CIRC14-code-softpot.js
@@ -0,0 +1,33 @@
+var five = require("johnny-five"),
+    board, mySoftPot, myLed;
+
+function getRGB(hue) {
+  var colors = [];
+  var r = five.Fn.constrain(five.Fn.map(hue, 0, 512, 255, 0), 0, 255);
+  var g = five.Fn.constrain(
+        five.Fn.map(hue, 0, 512, 0, 255), 0, 255) -
+      five.Fn.constrain(five.Fn.map(hue, 512, 1023, 0, 255),0,255);
+  var b = five.Fn.constrain(five.Fn.map(hue, 512, 1023, 0, 255), 0, 255);
+  colors[0] = r;
+  colors[1] = g;
+  colors[2] = b;
+  return colors;
+}
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  myLed = new five.Led.RGB([ 9, 10, 11 ]);
+
+  mySoftPot = new five.Sensor({
+    pin: "A0",
+    freq: 250
+  });
+  
+  mySoftPot.on("read", function( err, value ) {
+    console.log("read value",value);
+    var rgbColors = getRGB(value);
+    myLed.color(rgbColors);
+  });
+});

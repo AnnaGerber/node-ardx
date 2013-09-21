f75298d1190fefccb54eb6a23b61cbe7902f1e82
Anna Gerber <anna.m.gerber@gmail.com>
2013-09-21 19:00:15 +1000

    Code for circuit 3 - DC Motor

---
+++ code/CIRC03-code-motor.js
@@ -0,0 +1,28 @@
+var five = require("johnny-five"),
+    board, myMotor, led;
+
+board = new five.Board();
+
+board.on("ready", function() {
+  myMotor = new five.Motor({
+    pin: 9
+  });
+
+  // event handlers on start and stop
+  myMotor.on("start", function( err, timestamp ) {
+    console.log( "started", timestamp );
+
+    // stop after 2 seconds
+    board.wait(2000, function() {
+      myMotor.stop();
+    });
+  }); 
+
+  myMotor.on("stop", function( err, timestamp ) {
+    console.log( "stopped", timestamp );
+  });
+
+  // start motor - speed argument (between 0 and 255) is optional
+  myMotor.start(250);
+  
+});

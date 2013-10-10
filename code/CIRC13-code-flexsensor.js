05e5b0a174381c171d1c9cf3359b570276e5de2e
Anna Gerber <anna.m.gerber@gmail.com>
2013-10-11 06:25:33 +1000

    Ex 13 proof read and complete

---
+++ code/CIRC13-code-flexsensor.js
@@ -0,0 +1,11 @@
+var five = require("johnny-five");
+
+five.Board().on("ready", function(){
+  var flexSensor = new five.Sensor("A0");
+  var myServo = new five.Servo(9);
+
+  flexSensor.on("read", function(err, value){
+    var servoAngle = five.Fn.map(value, 50, 300, 0, 179);
+    myServo.move(servoAngle);
+  });
+});

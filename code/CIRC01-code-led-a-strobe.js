9419d35821da52a7b64ef67f7716947d7837ecda
KatieK <kkgithub@gmail.com>
2014-12-08 22:02:26 -0800

    Revert "Changed j5 object to five."
    This reverts commit 7d4396d74bf5a38574ce02fd3061379f5646ea6d.

--- code/CIRC01-code-led-a-strobe.js
+++ code/CIRC01-code-led-a-strobe.js
@@ -1,20 +1,20 @@
-var five = require("johnny-five");
+var j5 = require("johnny-five");
 var myBoard, myLed;
 
-myBoard = new five.Board();
+myBoard = new j5.Board();
 
 myBoard.on("ready", function() {
 
-  myLed = new five.Led(13)
+  myLed = new j5.Led(13)
 
   myLed.strobe( 1000 );
 
   // make myLED available as "led" in REPL
 
   // try "on", "off", "toggle", "strobe", "stop" (stops strobing)
-
+  
   this.repl.inject({
-    led: myLed
+  	led: myLed
   });
   console.log("You can interact with the RGB LED via the variable 'led' e.g. led.on();\n Hit control-D to exit.\n >> ");
 });

01606eeef20e3dd934afa79ce45236ee90a038ec
KatieK <kkgithub@gmail.com>
2015-02-22 20:35:36 -0800

    Ex 11: changed j5 to five.

--- code/CIRC11-code-relay.js
+++ code/CIRC11-code-relay.js
@@ -1,9 +1,9 @@
-var j5 = require("johnny-five");
-var myBoard = new j5.Board();
+var five = require("johnny-five");
+var myBoard = new five.Board();
 
 myBoard.on("ready", function() {
   var val = 0;
-  var relay = new j5.Relay(2);
+  var relay = new five.Relay(2);
   this.loop(1000, function() {
     if (val ^= 1) {
       relay.on();
